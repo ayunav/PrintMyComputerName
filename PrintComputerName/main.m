9866a38b1f7de7ad9f35d152d1dd8c4fe2a8fbd4
ayunav <ayunanyc@gmail.com>
2016-02-18 13:04:25 -0500

    Initial Commit

---
+++ PrintComputerName/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PrintComputerName
+//
+//  Created by Ayuna Vogel on 2/18/16.
+//  Copyright © 2016 Ayuna Vogel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
