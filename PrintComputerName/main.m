9bb4877973446e87ad27cf3a316c1e3dc33deb5f
ayunav <ayunanyc@gmail.com>
2016-02-18 15:31:20 -0500

    first commit

--- PrintComputerName/main.m
+++ PrintComputerName/main.m
@@ -10,8 +10,40 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        /*
+         Write a simple program to print out your computer's name.
+         
+         This program uses:
+         - class methods,
+         - instance methods,
+         - pointers,
+         - NSString,
+         - NSLog() function.
+         */
+        
+        /*
+         There's NSHost class that has information about your computer.
+         
+         Create an instance of NSHost object using the NSHost class method:
+         + (NSHost *)currentHost
+         that returns a pointer to the NSHost object and its address in computer's memory.
+         */
+        
+        NSHost *myComp = [NSHost currentHost];
+        
+        /*
+         Get the localized name of your computer using NSHost instance method:
+         - (NSString *)localizedName
+         that returns a pointer to an NSString object.
+         */
+        
+        NSString *myCompName = [myComp localizedName];
+        
+        // Print the name of your computer
+        
+        NSLog(@"%@", myCompName);
+    
     }
     return 0;
 }
